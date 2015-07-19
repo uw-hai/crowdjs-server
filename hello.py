abcbb6d7f498d7b7f59a1e08702a177da919ee10
Christopher Lin <christopher.h.lin@gmail.com>
2015-07-19 16:09:29 -0700

    Connection to MongoDB. Don't forget to add .env with MONGOLBA_URI and APP_SETTINGS

--- hello.py
+++ hello.py
@@ -1,8 +1,22 @@
 import os
 from flask import Flask
+from flask.ext.mongoengine import MongoEngine
 
 app = Flask(__name__)
+
+app.config.from_object(os.environ['APP_SETTINGS'])
+
+db = MongoEngine(app)
+
+class Requester(db.Document):
+    email = db.StringField(required=True)
+    
+test_requester = Requester(email='dan@weld.com')
+test_requester.save()
+
 
 @app.route('/')
 def hello():
     return 'Hello World!'
+
+
