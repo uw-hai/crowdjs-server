4e9e63d486163e1b8843fd24d9edf81d83531086
Christopher Lin <christopher.h.lin@gmail.com>
2015-07-19 16:12:40 -0700

    forgot to add config,py

---
+++ config.py
@@ -0,0 +1,24 @@
+import os
+
+class Config(object):
+    DEBUG = False
+    TESTING = False
+
+    mongolab_uri = os.environ['MONGOLAB_URI'].split('/')
+    (dbuser, dbpass_host, port) = mongolab_uri[2].split(':')
+    (dbpass, host) = dbpass_host.split('@')
+    dbname = mongolab_uri[3]
+
+    MONGODB_SETTINGS = {    
+        'db': dbname,
+        'host': host,
+        'port': int(port),
+        'username' : dbuser,
+        'password' : dbpass}
+
+class DevelopmentConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+class Production(Config):
+    DEBUG = False
