15fa507248a35fb0388f3cedc646fb6d82bb6ec2
Christopher Lin <christopher.h.lin@gmail.com>
2015-07-20 16:08:52 -0700

    add answer api

--- app.py
+++ app.py
@@ -19,12 +19,24 @@ def hello():
     test_requester.save()
     return 'Hello World! Dan Weld has been added to the DB!'
 
-@app.route('/add_question')
-def add_question():
-    test_question = schema.question.Question(name='testing')
+@app.route('/add_test_question')
+def add_test_question():
+    test_requester = schema.requester.Requester.objects.get_or_404(
+        email='dan@weld.com')
+    test_question = schema.question.Question(name='testing',
+                                             requester = test_requester)
     test_question.save()
     return 'Test question added to DB'
 
+@app.route('/add_test_worker')
+def add_test_worker():
+    test_worker = schema.worker.Worker(turk_id='dan_weld_the_worker')
+    test_worker.save()
+    return 'Test worker added to DB'
+    
 
 from api.question import QuestionApi
 api.add_resource(QuestionApi, '/questions/<question_id>')
+
+from api.answer_api import AnswerApi
+api.add_resource(AnswerApi, '/api/add_answer')
