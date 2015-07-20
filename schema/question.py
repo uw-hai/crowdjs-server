7634c4c4e666b50c1e15432e700acd741c6e3711
Christopher Lin <christopher.h.lin@gmail.com>
2015-07-20 15:47:57 -0700

    must commit before merge

--- schema/question.py
+++ schema/question.py
@@ -1,10 +1,11 @@
 from flask.ext.security import RoleMixin
 from app import db
 from answer import Answer
+from requester import Requester
 
 class Question(db.Document):
     name = db.StringField(max_length=80, unique=True)
     description = db.StringField(max_length=255)
     worker_answers = db.ListField(db.ReferenceField(Answer), default=[])
     valid_answers = db.ListField(db.StringField, default=[])
-
+    requester = db.ReferenceField(Requester)
