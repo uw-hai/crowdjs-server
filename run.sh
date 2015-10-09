f8055b5159757bb766bfa59412be32e4ecd38b25
Seth Vanderwilt <sethv1@cs.washington.edu>
2015-10-09 14:43:31 -0700

    fixed run.sh args

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@
 # Alternatively, use a command like 'heroku local'.
 # Sample usage: ./run .env -b 0.0.0.0:8000.
 
-env $(cat $1 | xargs) gunicorn app:app ${@:1}
+env $(cat $1 | xargs) gunicorn app:app ${@:2}
