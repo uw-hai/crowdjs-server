335548795b5e7570a1a3062d684b5292a4b25b27
Seth Vanderwilt <sethv1@cs.uw.edu>
2016-02-05 17:46:56 -0800

    improved comments describing inference_results fields

--- schema/worker.py
+++ schema/worker.py
@@ -3,3 +3,8 @@ from app import db
 class Worker(db.Document):
     platform_id = db.StringField()
     platform_name = db.StringField()
+
+    # Mapping inference strategy -> result
+    # Example format:
+    # strategy : {'timestamp' : timestamp, 'skill' : skill_estimate}
+    inference_results = db.DictField()
