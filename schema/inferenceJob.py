60efa0af112f5ac01067b49849db3319c4cd1219
Seth Vanderwilt <sethv1@cs.uw.edu>
2016-03-16 17:48:06 -0700

    added schema for task-level inference jobs (& results)

---
+++ schema/inferenceJob.py
@@ -0,0 +1,18 @@
+from app import db
+
+class InferenceJob(db.Document):
+    """
+    Stores results of some inference/aggregation algorithm run on a task
+    """
+
+    #main fields
+    requester = db.ReferenceField('Requester')
+    task = db.ReferenceField('Task')
+    strategy = db.StringField(max_length=80) # algorithm to run
+    results = db.DynamicField() # NOTE results can be written in any format for now
+    status = db.StringField(choices=('Created','Running','Completed','Killed'), default='Created')
+
+    #optional
+    additional_params = db.StringField(max_length=80, default="") # additional arguments for algorithm
+
+    #TODO other possible fields: timeout, created_time, completed_time, run_time, ...
